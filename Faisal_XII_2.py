ff90fdaa44bb52fe1585a05978177645550739f9
Arrasyid71 <145412863+Arrasyid71@users.noreply.github.com>
2024-08-19 08:35:02 +0700

    Create Faisal_XII_2.py

---
+++ Faisal_XII_2.py
@@ -0,0 +1,31 @@
+nama = "Alice"
+print("Nama:", nama)
+
+umur = 25
+print("Umur:", umur)
+
+tinggi = 165.5
+print("Tinggi:", tinggi)
+
+is_student = True
+print("Apakah seorang pelajar?", is_student)
+
+hobi = ["Membaca", "Menulis", "Bersepeda"]
+print("Hobi:", hobi)
+
+koordinat = (10.5, 20.2)
+print("Koordinat:", koordinat)
+
+biodata = {
+    "Nama": "Alice",
+    "Umur": 25,
+    "Tinggi": 165.5,
+    "Pelajar": True
+}
+print("Biodata:", biodata)
+
+pekerjaan = None
+print("Pekerjaan:", pekerjaan)
+
+umur = 26
+print("Umur baru:", umur)
